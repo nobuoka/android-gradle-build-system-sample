b8b418224fb7d938897b7b8d3b8c3fa727c3dda4
nobuoka <nobuoka@vividcode.info>
2013-06-16 23:23:23 +0900

    Volley の RequestQueue を Activity 内で使えるように

--- GradleBuildSystemSample/src/main/java/info/vividcode/android/app/gradlebuildsystemsample/MainActivity.java
+++ GradleBuildSystemSample/src/main/java/info/vividcode/android/app/gradlebuildsystemsample/MainActivity.java
@@ -1,17 +1,51 @@
 package info.vividcode.android.app.gradlebuildsystemsample;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    /** この Activity の running 中 (onStart から onStop まで) ずっと動いている RequestQueue */
+    private RequestQueue mRequestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Activity 作成時に RequestQueue オブジェクトを生成
+        // (`newRequestQueue` メソッドで作られた RequestQueue のバックグラウンドスレッドは開始されている)
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // Activity 再開時にバックグラウンドスレッドを開始する
+        mRequestQueue.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Activity 停止時にはバックグラウンドスレッドを止める
+        mRequestQueue.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 最終処理として念のためバックグラウンドスレッドを止める
+        // (既に停止している場合に `stop()` しても問題ない)
+        if (mRequestQueue != null) mRequestQueue.stop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
